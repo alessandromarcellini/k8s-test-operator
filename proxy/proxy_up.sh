25f2afac3508d53e4b863eb91e5147712d159a73
Alessandro Marcellini <alessandromarcellini49@gmail.com>
2026-05-28 16:11:55 +0200

    Added prometheus and grafana to the cluster, made up.sh in order to bring everything up in a single command

--- proxy/proxy_up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl delete pod proxy
-
-docker build -t proxy:latest .
-
-minikube image load proxy:latest
-
-kubectl apply -f ../manifests/proxy.yaml
---
+++ up.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# delete everything pre-existing
+kubectl delete -f ./manifests/proxy.yaml --ignore-not-found
+kubectl delete -f ./manifests/prometheus.yaml --ignore-not-found
+kubectl delete -f ./manifests/grafana.yaml --ignore-not-found
+kubectl delete -f ./manifests/grafana-dashboards-configmap.yaml --ignore-not-found
+
+
+#create proxy
+cp ~/.minikube/profiles/minikube/client.crt proxy/cert/
+cp ~/.minikube/profiles/minikube/client.key proxy/cert/
+
+docker build -t proxy:latest ./proxy
+
+minikube image load proxy:latest
+
+kubectl apply -f manifests/proxy.yaml
+
+#create namespace for prometheus and grafana
+kubectl create namespace monitoring
+
+# create prometheus
+kubectl apply -f manifests/prometheus.yaml
+
+
+# create grafana
+kubectl apply -f manifests/grafana-dashboards-configmap.yaml # dashboards in json
+kubectl apply -f manifests/grafana.yaml
+
+# wait for pods to be ready
+kubectl wait --for=condition=ready pod -l app=proxy -n default --timeout=60s
+kubectl wait --for=condition=ready pod -l app=prometheus -n monitoring --timeout=60s
+kubectl wait --for=condition=ready pod -l app=grafana -n monitoring --timeout=60s
+
+# portforwarding for proxy to be reachable by external elements from the cluster (I'm using the operator outside the cluster using go run)
+kubectl port-forward -n monitoring svc/prometheus 9090:9090 &
+kubectl port-forward -n monitoring svc/grafana 3000:3000 &
+kubectl port-forward -n default svc/proxy 8080:8080
+
